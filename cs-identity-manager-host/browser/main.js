54be8bee25172237d83767f6c963273b4188806e
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-27 12:26:50 -0500

    set up assets

---
+++ cs-identity-manager-host/browser/main.js
@@ -0,0 +1,16 @@
+import './css/style.scss'
+import '@fortawesome/fontawesome-free/attribution'
+import "./images/beer.svg";
+import "./images/join.svg";
+
+const sign_in_btn = document.querySelector('#sign-in-btn');
+const sign_up_btn = document.querySelector('#sign-up-btn');
+const container = document.querySelector('.full-height');
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add('sign-up-mode');
+});
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove('sign-up-mode');
+});
