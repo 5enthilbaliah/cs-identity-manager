3a531f23c00cdbe2c53e6dc373f0307faef22028
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-27 21:07:37 -0500

    style sheet with css finalized - need to change to scss

--- cs-identity-manager-host/browser/main.js
+++ cs-identity-manager-host/browser/main.js
@@ -5,13 +5,15 @@ import "./images/join.svg";
 
 const sign_in_btn = document.querySelector('#sign-in-btn');
 const sign_up_btn = document.querySelector('#sign-up-btn');
-
+const nav_bar = document.querySelector('.navbar');
 const container = document.querySelector('.login-container');
 
 sign_up_btn.addEventListener('click', () => {
     container.classList.add('sign-up-mode');
+    nav_bar.classList.add('sign-up-mode');
 });
 
 sign_in_btn.addEventListener('click', () => {
     container.classList.remove('sign-up-mode');
+    nav_bar.classList.remove('sign-up-mode');
 });
