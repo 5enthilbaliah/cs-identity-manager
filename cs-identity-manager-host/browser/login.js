5e8468bab15308f626798c1e092437982c9ea6ef
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-08-28 00:38:20 -0500

    multi script working

---
+++ cs-identity-manager-host/browser/login.js
@@ -0,0 +1,16 @@
+﻿import './css/login.scss'
+
+const sign_in_btn = document.querySelector('#sign-in-btn');
+const sign_up_btn = document.querySelector('#sign-up-btn');
+const nav_bar = document.querySelector('.navbar');
+const container = document.querySelector('.login-container');
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add('sign-up-mode');
+    nav_bar.classList.add('sign-up-mode');
+});
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove('sign-up-mode');
+    nav_bar.classList.remove('sign-up-mode');
+});
