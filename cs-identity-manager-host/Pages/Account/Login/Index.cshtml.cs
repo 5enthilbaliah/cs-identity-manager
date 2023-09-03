5956174b4bcebbab1e1dd2d3199b82eb60beeca6
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-09-03 16:32:55 -0500

    Login succeeding

--- cs-identity-manager-host/Pages/Account/Login/Index.cshtml.cs
+++ cs-identity-manager-host/Pages/Account/Login/Index.cshtml.cs
@@ -2,6 +2,7 @@
 
 using Domain;
 
+using Duende.IdentityServer;
 using Duende.IdentityServer.Services;
 using Duende.IdentityServer.Stores;
 
@@ -10,6 +11,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 [SecurityHeaders]
 [AllowAnonymous]
@@ -21,7 +24,9 @@ public class LoginPageModel : PageModel
     private readonly IAuthenticationSchemeProvider _schemeProvider;
     private readonly SignInManager<AmritaUser> _signInManager;
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public LoginPageModel(
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         IIdentityServerInteractionService interaction,
         IAuthenticationSchemeProvider schemeProvider,
         IIdentityProviderStore identityProviderStore,
@@ -35,9 +40,10 @@ public class LoginPageModel : PageModel
         _signInManager = signInManager;
     }
 
-    [BindProperty] public SignInModel? SignInInfo { get; set; }
+    public LoginViewModel View { get; set; }
+    [BindProperty] public SignInModel SignInInfo { get; set; }
 
-    [BindProperty] public SignUpModel? SignUpInfo { get; set; }
+    [BindProperty] public SignUpModel SignUpInfo { get; set; }
 
     [BindProperty] public bool IsSignInSelected { get; set; }
 
@@ -45,6 +51,13 @@ public class LoginPageModel : PageModel
     {
         IsSignInSelected = true;
         await BuildModelAsync(returnUrl);
+
+        if (View.IsExternalLoginOnly)
+        {
+            // we only have one option for logging in and it's an external provider
+            return RedirectToPage("/ExternalLogin/Challenge", new { scheme = View.ExternalLoginScheme, returnUrl });
+        }
+
         return Page();
     }
 
@@ -59,7 +72,14 @@ public class LoginPageModel : PageModel
         ModelState.RemoveMany("Email", "FullName", "UserName", "Password");
         if (ModelState.IsValid)
         {
-            await Task.CompletedTask;
+            // check if we are in the context of an authorization request
+            var context = await _interaction.GetAuthorizationContextAsync(SignInInfo.ReturnUrl);
+            var user = await _signInManager.UserManager.FindByNameAsync(SignInInfo.UserName);
+            if (user is not null && (await _signInManager.CheckPasswordSignInAsync(user, SignInInfo.Password, false))
+                == SignInResult.Success)
+            {
+                ;
+            }
         }
 
         IsSignInSelected = true;
@@ -73,13 +93,71 @@ public class LoginPageModel : PageModel
         {
             await Task.CompletedTask;
         }
-        
+
         return Page();
     }
 
     private async Task BuildModelAsync(string returnUrl)
     {
         SignInInfo = new SignInModel { ReturnUrl = returnUrl };
-        await Task.CompletedTask;
+        SignUpInfo = new SignUpModel();
+
+        var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
+        if (context?.IdP != null && await _schemeProvider.GetSchemeAsync(context.IdP) != null)
+        {
+            var local = context.IdP == IdentityServerConstants.LocalIdentityProvider;
+
+            // this is meant to short circuit the UI and only trigger the one external IdP
+            View = new LoginViewModel { EnableLocalLogin = local };
+
+            SignInInfo.UserName = context.LoginHint ?? string.Empty;
+
+            if (!local)
+            {
+                View.ExternalProviders = new[]
+                {
+                    new LoginViewModel.ExternalProvider { AuthenticationScheme = context.IdP ?? "Bearer"}
+                };
+            }
+
+            return;
+        }
+
+        var schemes = await _schemeProvider.GetAllSchemesAsync();
+
+        var providers = schemes
+            .Where(x => x.DisplayName != null)
+            .Select(x => new LoginViewModel.ExternalProvider
+            {
+                DisplayName = x.DisplayName ?? x.Name, AuthenticationScheme = x.Name
+            }).ToList();
+
+        var dynamicSchemes = (await _identityProviderStore.GetAllSchemeNamesAsync())
+            .Where(x => x.Enabled)
+            .Select(x => new LoginViewModel.ExternalProvider
+            {
+                AuthenticationScheme = x.Scheme, DisplayName = x.DisplayName ?? "default"
+            });
+        providers.AddRange(dynamicSchemes);
+
+
+        var allowLocal = true;
+        var client = context?.Client;
+        if (client != null)
+        {
+            allowLocal = client.EnableLocalLogin;
+            if (client.IdentityProviderRestrictions.Any())
+            {
+                providers = providers.Where(provider =>
+                    client.IdentityProviderRestrictions.Contains(provider.AuthenticationScheme)).ToList();
+            }
+        }
+
+        View = new LoginViewModel
+        {
+            AllowRememberLogin = LoginOptions.AllowRememberLogin,
+            EnableLocalLogin = allowLocal && LoginOptions.AllowLocalLogin,
+            ExternalProviders = providers.ToArray()
+        };
     }
 }
---
+++ cs-identity-manager-host/Pages/Account/Login/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Amrita.IdentityManager.Host.Pages.Account.Login;
+
+public class LoginViewModel
+{
+    public bool AllowRememberLogin { get; set; } = true;
+    public bool EnableLocalLogin { get; set; } = true;
+
+    public IEnumerable<ExternalProvider> ExternalProviders { get; set; } = Enumerable.Empty<ExternalProvider>();
+    public IEnumerable<ExternalProvider> VisibleExternalProviders => ExternalProviders.Where(x => !String.IsNullOrWhiteSpace(x.DisplayName));
+
+    public bool IsExternalLoginOnly => EnableLocalLogin == false && ExternalProviders?.Count() == 1;
+    public string? ExternalLoginScheme => IsExternalLoginOnly ? ExternalProviders?.SingleOrDefault()?.AuthenticationScheme : null;
+        
+    public class ExternalProvider
+    {
+        public string DisplayName { get; set; } = null!;
+        public string AuthenticationScheme { get; set; } = null!;
+    }
+}
