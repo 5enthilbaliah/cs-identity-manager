92cb7f395696470d4d636c7f3588ae4ba1e8c278
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-09-03 14:23:36 -0500

    Proceeding with partial usage - next validation on fields

--- cs-identity-manager-host/Pages/Account/Login/Index.cshtml.cs
+++ cs-identity-manager-host/Pages/Account/Login/Index.cshtml.cs
@@ -1,6 +1,13 @@
 ﻿namespace Amrita.IdentityManager.Host.Pages.Account.Login;
 
+using Domain;
+
+using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Stores;
+
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,11 +15,67 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 [AllowAnonymous]
 public class LoginPageModel : PageModel
 {
+    private readonly IEventService _events;
+    private readonly IIdentityProviderStore _identityProviderStore;
+    private readonly IIdentityServerInteractionService _interaction;
+    private readonly IAuthenticationSchemeProvider _schemeProvider;
+    private readonly SignInManager<AmritaUser> _signInManager;
+
+    public LoginPageModel(
+        IIdentityServerInteractionService interaction,
+        IAuthenticationSchemeProvider schemeProvider,
+        IIdentityProviderStore identityProviderStore,
+        IEventService events,
+        SignInManager<AmritaUser> signInManager)
+    {
+        _interaction = interaction;
+        _schemeProvider = schemeProvider;
+        _identityProviderStore = identityProviderStore;
+        _events = events;
+        _signInManager = signInManager;
+    }
+
     [BindProperty] public SignInModel? SignInInfo { get; set; }
 
     [BindProperty] public SignUpModel? SignUpInfo { get; set; }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGet(string returnUrl)
     {
+        await BuildModelAsync(returnUrl);
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        await Task.CompletedTask;
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostSignIn()
+    {
+        ModelState.RemoveMany("Email", "FullName", "UserName", "Password");
+        if (ModelState.IsValid)
+        {
+            await Task.CompletedTask;
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostSignUp()
+    {
+        ModelState.RemoveMany("UserName", "Password");
+        if (ModelState.IsValid)
+        {
+            await Task.CompletedTask;
+        }
+
+        return Page();
+    }
+
+    private async Task BuildModelAsync(string returnUrl)
+    {
+        SignInInfo = new SignInModel { ReturnUrl = returnUrl };
+        await Task.CompletedTask;
     }
 }
---
+++ cs-identity-manager-host/Pages/ModelStateExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Amrita.IdentityManager.Host.Pages;
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+public static class ModelStateExtensions
+{
+    public static void RemoveMany(this ModelStateDictionary modelState, params string[] keys)
+    {
+        foreach (var key in keys)
+        {
+            modelState.Remove(key);
+        }
+    }
+}
