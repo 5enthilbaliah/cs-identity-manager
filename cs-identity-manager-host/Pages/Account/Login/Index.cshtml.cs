2d6518f558e32f488383137762bdd343e2d4fb5f
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-09-04 14:17:26 -0500

    update logo

--- cs-identity-manager-host/Pages/Account/Login/Index.cshtml.cs
+++ cs-identity-manager-host/Pages/Account/Login/Index.cshtml.cs
@@ -4,15 +4,7 @@ using Application.Login;
 using Application.Login.Interfaces;
 using Application.Login.Models;
 
-using Domain;
-
-using Duende.IdentityServer;
-using Duende.IdentityServer.Services;
-using Duende.IdentityServer.Stores;
-
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -20,27 +12,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 [AllowAnonymous]
 public class LoginPageModel : PageModel
 {
-    private readonly IEventService _events;
-    private readonly IIdentityProviderStore _identityProviderStore;
-    private readonly IIdentityServerInteractionService _interaction;
     private readonly ILoginService _loginService;
-    private readonly IAuthenticationSchemeProvider _schemeProvider;
-    private readonly SignInManager<AmritaUser> _signInManager;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public LoginPageModel(
+    public LoginPageModel(ILoginService loginService)
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        IIdentityServerInteractionService interaction,
-        IAuthenticationSchemeProvider schemeProvider,
-        IIdentityProviderStore identityProviderStore,
-        IEventService events,
-        SignInManager<AmritaUser> signInManager, ILoginService loginService)
     {
-        _interaction = interaction;
-        _schemeProvider = schemeProvider;
-        _identityProviderStore = identityProviderStore;
-        _events = events;
-        _signInManager = signInManager;
         _loginService = loginService;
     }
 
@@ -94,10 +71,12 @@ public class LoginPageModel : PageModel
         }
 
         // something went wrong, show form with error
-        var (signIn, view) =await _loginService.BindModelsAsync(SignInInfo.ReturnUrl);
+#pragma warning disable CS8604 // Possible null reference argument.
+        var (signIn, view) = await _loginService.BindModelsAsync(SignInInfo.ReturnUrl);
+#pragma warning restore CS8604 // Possible null reference argument.
         SignInInfo = signIn;
         View = view;
-        
+
         IsSignInSelected = true;
         return Page();
     }
