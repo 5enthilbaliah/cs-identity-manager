3d51672e9c32d47abb9dd8e01b728ed2b23d8a14
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-09-06 23:42:45 -0500

    moving to presentation layer

--- cs-identity-manager-host/Installers/MvcServiceInstaller.cs
+++ cs-identity-manager-host/Installers/MvcServiceInstaller.cs
@@ -4,12 +4,15 @@ using Enums;
 
 using Interfaces;
 
+using Presentation;
+
 public class MvcServiceInstaller : IServiceInstaller
 {
     public InstallationOrder Order => InstallationOrder.Mvc;
 
     public void Install(IServiceCollection services, IConfiguration configuration, string environment)
     {
-        services.AddRazorPages();
+        services.AddRazorPages()
+            .AddApplicationPart(typeof(PresentationModule).Assembly);
     }
 }
