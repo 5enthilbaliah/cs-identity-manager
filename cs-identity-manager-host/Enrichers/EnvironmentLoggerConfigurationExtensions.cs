05fdabf0c3d359718a848009cf58c3729bd43c7b
5enthilbaliah <ternaryoperator.fork@gmail.com>
2023-09-10 17:13:22 -0500

    working on log enhancements

---
+++ cs-identity-manager-host/Enrichers/EnvironmentLoggerConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Serilog;
+
+using Configuration;
+
+using Enrichers;
+
+public static class EnvironmentLoggerConfigurationExtensions
+{
+    public static LoggerConfiguration WithMachineName(
+        this LoggerEnrichmentConfiguration enrichmentConfiguration)
+    {
+        if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+        return enrichmentConfiguration.With<MachineNameEnricher>();
+    }
+    
+    
+}
